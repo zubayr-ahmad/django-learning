7ce71267a04afd45a3413a9be9710b294dd3c1d5
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-04-23 13:02:09 +0500

    templates and urls routing

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("room/", views.room, name="room"),
+    # name=room is used inside the url jinja inside the html, so even if our actual url change, we can still access it using the name=room and does not need to change anything
+    path("room_page/<str:pk>/", views.room, name="room"), # str represents string and pk is primary key, although we will be using int but we have written str here
+
+    
 ]
